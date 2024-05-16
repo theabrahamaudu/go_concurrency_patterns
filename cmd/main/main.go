d45ea14dc86c8bfba8cf9b0331a7f3d8a9c2319f
Abraham Audu <ab6ril@gmail.com>
2024-05-16 13:23:20 +0100

    Pipelines

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"time"
 	// "time"
 )
 
@@ -105,3 +104,43 @@ func main() {
 }
 */
 
+// Pipelines
+
+func sliceToChannel(nums []int) <-chan int {
+	out := make(chan int)
+
+	go func() {
+		for _, n := range nums {
+			out <-n
+		}
+
+		close(out)
+	}()
+	return out
+}
+
+
+func sq(in <-chan int) <-chan int {
+	out := make(chan int)
+
+	go func() {
+		for n := range in {
+			out <- n * n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func main() {
+	// input
+	nums := []int{2, 3, 4, 7, 1}
+	// stage 1
+	dataChannel := sliceToChannel(nums)
+	// stage 2
+	finalChannel := sq(dataChannel)
+	// stage 3
+	for n := range finalChannel {
+		fmt.Println(n)
+	}
+}
