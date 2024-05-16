a93c041f9759162dc660d6abcbfe88532e3153e6
Abraham Audu <ab6ril@gmail.com>
2024-05-16 12:49:12 +0100

    Done channel pattern

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -64,6 +64,7 @@ import (
 // }
 
 // For-Select Loop Ex 2
+/*
 func main() {
 	go func() {
 		for {
@@ -76,3 +77,31 @@ func main() {
 
 	time.Sleep(time.Second * 3)
 }
+*/
+
+/*
+// Done Channel
+func doWork(done <-chan bool) {
+	for {
+		select {
+		case <-done:
+			return
+		default:
+			fmt.Println("DOING WORK")
+		}
+	}
+}
+
+
+func main() {
+
+	done := make(chan bool)
+
+	go doWork(done)
+
+	time.Sleep(time.Second * 3)
+
+	close(done)
+}
+*/
+
