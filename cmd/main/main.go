f303c9f846946a67a506b75f52700c74151f36c5
Abraham Audu <ab6ril@gmail.com>
2024-05-16 11:19:32 +0100

    select primitive

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -27,12 +27,21 @@ import (
 // ===== Channels =====
 func main() {
 	myChannel := make(chan string)
+	anotherChannel := make(chan string)
 
 	go func() {
 		myChannel <- "data"
 	}()
 
-	msg := <- myChannel
 
-	fmt.Println(msg)
+	go func() {
+		anotherChannel <- "cow"
+	}()
+
+	select {
+	case msgFromMyChannel := <- myChannel:
+		fmt.Println(msgFromMyChannel)
+	case msgFromAnotherChannel := <- anotherChannel:
+		fmt.Println(msgFromAnotherChannel)
+	}
 }
